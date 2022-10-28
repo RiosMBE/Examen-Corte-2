22294057f6698eefdc47c031ba6a2a6e96a77efe
Brian Rios <briosm0601@gmail.com>
2022-10-28 17:53:45 -0600

    Evento change

--- JS/operaciones.js
+++ JS/operaciones.js
@@ -1,3 +1,18 @@
+var pesoMexicano = 19.88;
+var dolarAmericano = 1;
+var dolarCanadience = 1.35;
+var Euro = 0.99;
+
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+input.addEventListener('change', updateValue);
+function updateValue(e){
+    log.textContent = e.target.value;
+}
+
 function Calcular(){
-    let pesoMexicano
+    let pesoMexicano = document.getElementById('PesoMexicano').value;
+    let dolarAmericano = document.getElementById('dolarAmericano').value;
+    let dolarCanadiense = document.getElementById('dolarCanadience').value;
+    let Euro = document.getElementById('Euro').value;
 }
